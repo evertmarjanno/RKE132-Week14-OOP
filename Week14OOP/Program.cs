51fb1310ff8c4b1f84ec5b70381c8df27fc69faf
Marjanno Evert <marjanno.evert@tktk.ee>
2024-08-28 21:13:11 +0300

    Week 14 Doggo OOP

---
+++ Week14OOP/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.WriteLine("Name your dog:");
+string myDogName = Console.ReadLine()!;
+
+Dog myDog = new(myDogName);
+Dog neighboursDog = new("Good girl");
+
+Console.WriteLine($"My dogs name is {myDog.Name}.");
+Console.WriteLine($"My neighbours dogs name is {neighboursDog.Name}.");
+
+myDog.Rename("Bad boy");
+
+while (myDog.LevelOfHappiness != 5)
+{
+    myDog.Bark();
+}
+
+myDog.WagTail();
+
+class Dog(string name)
+{
+    private string _name = name;
+    private int _levelOfHappiness = 0;
+
+    public string Name
+    {
+        get { return _name; }
+    }
+
+    public int LevelOfHappiness
+    {
+        get { return _levelOfHappiness; }
+    }
+
+    public void Rename(string newName)
+    {
+        _name = newName;
+        Console.WriteLine($"The dog has been renamed to: {newName}");
+    }
+
+    public void Bark()
+    {
+        Console.WriteLine("Woof-woof!");
+        _levelOfHappiness++;
+    }
+
+    public void WagTail()
+    {
+        Console.WriteLine("Wiggle-wiggle!");
+    }
+}
